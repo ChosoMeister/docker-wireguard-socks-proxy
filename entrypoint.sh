734de366bead087c5cedd6dcf99414681a22950b
Mustafa T <mustafa@Mustafas-MacBook-Pro.local>
2024-12-17 11:54:48 +0330

    fix some error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-set -e  # Exit on error
+set -e
 
-# Dynamically detect the WireGuard interface name
+# Extract WireGuard interface name
 ifname=$(basename $(ls -1 /etc/wireguard/*.conf | head -1) .conf)
 
-# Bring up the WireGuard interface with the configuration file
+# Bring up WireGuard interface
 wg-quick up /etc/wireguard/$ifname.conf
 
-# Ensure ip6tables is properly set up for IPv6 traffic
+# Flush existing IPv6 rules to avoid conflicts
+ip6tables -F
 ip6tables -P FORWARD ACCEPT
 ip6tables -A FORWARD -i $ifname -j ACCEPT
 ip6tables -A FORWARD -o $ifname -j ACCEPT
 
-# Replace placeholder in SOCKS5 config with the actual WireGuard interface
+# Replace placeholder in SOCKS5 config
 sed -i'' -e "s/__replace_me_ifname__/$ifname/" /etc/sockd.conf
 
-# Start the Dante SOCKS5 proxy server
+# Start Dante SOCKS5 proxy
 /usr/sbin/sockd -f /etc/sockd.conf
